8257a79a45c7bd14de84551b0ecafc9a63c96f24
ceastld <1950946176@qq.com>
2024-03-09 01:31:25 +0800

    Revert "1"
    This reverts commit df168c052542c20465bb84722ddc4f5eabade12f.

--- install.sh
+++ install.sh
@@ -2,21 +2,22 @@
 urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 
 # username and password input
-echo -e "\nIf you do not have an account you can register at https://flame.is.tue.mpg.de/ following the installation instruction."
-read -p "Username (FLAME):" username
-read -p "Password (FLAME):" password
-username=$(urle $username)
-password=$(urle $password)
+# echo -e "\nIf you do not have an account you can register at https://flame.is.tue.mpg.de/ following the installation instruction."
+# read -p "Username (FLAME):" username
+# read -p "Password (FLAME):" password
+# username=$(urle $username)
+# password=$(urle $password)
 
-echo -e "\nDownloading FLAME..."
-mkdir -p data/FLAME2020/
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=flame&sfile=FLAME2020.zip&resume=1' -O './FLAME2020.zip' --no-check-certificate --continue
-unzip FLAME2020.zip -d data/FLAME2020/
-rm -rf FLAME2020.zip
+# echo -e "\nDownloading FLAME..."
+# mkdir -p data/FLAME2020/
+# wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=flame&sfile=FLAME2020.zip&resume=1' -O './FLAME2020.zip' --no-check-certificate --continue
+# unzip FLAME2020.zip -d data/FLAME2020/
+# rm -rf FLAME2020.zip
 
 echo -e "\nDownloading MICA..."
 mkdir -p data/pretrained/
 wget -O data/pretrained/mica.tar "https://keeper.mpdl.mpg.de/f/db172dc4bd4f4c0f96de/?dl=1"
+
 
 # https://github.com/deepinsight/insightface/issues/1896
 # Insightface has problems with hosting the models
